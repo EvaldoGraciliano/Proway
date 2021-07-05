83b5158140e082f2594a7576daf026cc335da013
Ellian Aragão Dias <ellian.aragao@gmail.com>
2021-07-05 13:39:49 -0300

    add js css icon-svg elements

---
+++ [SP01]-introducao-ao-credito/introducao-credito.js
@@ -0,0 +1,15 @@
+function message() {
+  alert('volta pra página inicial');
+}
+
+function titleClick() {
+  alert('encaminha menu de crédito');
+}
+
+function buttonFaleConosco() {
+  alert('encaminha para chat fale conosco');
+}
+
+function buttonMostrarCredito() {
+  alert('mostra os créditos');
+}
