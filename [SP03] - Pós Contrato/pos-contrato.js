64a1ae152cef269092239bda7807c2820e465fd5
Ellian Aragão Dias <ellian.aragao@gmail.com>
2021-07-06 23:31:38 -0300

    add sp03 completa

---
+++ [SP03] - Pós Contrato/pos-contrato.js
@@ -0,0 +1,16 @@
+function message() {
+  alert('volta pra página inicial');
+}
+
+function titleClick() {
+  alert('encaminha menu de crédito');
+}
+
+function buttonVisualizar() {
+  alert('saldo mudou de visibilidade');
+}
+
+function buttonRenegocie(event) {
+  event.preventDefault();
+  alert('Renegocie seu crédito');
+}
